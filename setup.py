f2f68a38e15f7fe8a783efe6d46a7ad3500aae51
palewire <ben.welsh@gmail.com>
2018-06-18 09:55:07 -0700

    v0.0.4

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from distutils.core import Command
 
 setup(
     name='cpi',
-    version='0.0.3',
+    version='0.0.4',
     description="Quickly adjust U.S. dollars for inflation using the Consumer Price Index (CPI)",
     author='Ben Welsh',
     author_email='ben.welsh@gmail.com',
     url='http://www.github.com/datadesk/cpi',
     license="MIT",
     packages=("cpi",),
+    include_package_data=True,
+    zip_safe=False,  # because we're including static files
     install_requires=("",),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
