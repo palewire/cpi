d0ab7525e2e3b43c7eb2eb11994e5d8322502d00
palewire <ben.welsh@gmail.com>
2018-06-30 12:24:22 -0700

    Added a warning log as well

--- cpi/__init__.py
+++ cpi/__init__.py
@@ -11,7 +11,11 @@ from .download import Downloader
 from .data import cpi_by_year, cpi_by_month
 from .errors import CPIDoesNotExist, StaleDataWarning
 
-# The default series is the CPI-U
+# Configure a logger
+import logging
+logger = logging.getLogger(__name__)
+
+# set the default series to the CPI-U
 DEFAULT_SERIES = "CUUR0000SA0"
 
 # Establish the range of data available
@@ -30,6 +34,7 @@ DAYS_SINCE_LATEST_YEAR = (date.today() - date(LATEST_YEAR, 1, 1)).days
 # If it's more than two and a half years out of date, raise a warning.
 if DAYS_SINCE_LATEST_YEAR > (365*2.25) or DAYS_SINCE_LATEST_MONTH > 60:
     warnings.warn(StaleDataWarning())
+    logger.warn("CPI data is out of date. To accurately inflate to today's dollars, you must run `cpi.update()`.")
 
 
 def get(year_or_month, series=DEFAULT_SERIES):
