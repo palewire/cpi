0c67aae7fa93b01396a00a930ab048b0aac8f6d2
palewire <b@palewi.re>
2022-02-15 08:19:40 -0800

    Add github testing

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ setup(
     include_package_data=True,
     zip_safe=False,  # because we're including static files
     install_requires=(
-        "requests>=2.20.0",
-        "click>=6.7",
-        "python-dateutil>=2.7.3",
-        "pandas>=0.23.1",
+        "requests",
+        "click",
+        "python-dateutil",
+        "pandas",
     ),
     entry_points="""
         [console_scripts]
@@ -27,10 +27,11 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
     ],
 )
