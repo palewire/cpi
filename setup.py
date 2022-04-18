121b3de38cbdc5b44d72ea546b47eaadb6dce1e1
palewire <b@palewi.re>
2022-04-18 08:30:25 -0700

    Work in setuptools-scm

--- setup.py
+++ setup.py
@@ -14,9 +14,35 @@ def read(file_name):
         return f.read()
 
 
+def version_scheme(version):
+    """
+    Version scheme hack for setuptools_scm.
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+    If that issue is resolved, this method can be removed.
+    """
+    import time
+
+    from setuptools_scm.version import guess_next_version
+
+    if version.exact:
+        return version.format_with("{tag}")
+    else:
+        _super_value = version.format_next_version(guess_next_version)
+        now = int(time.time())
+        return _super_value + str(now)
+
+
+def local_version(version):
+    """
+    Local version scheme hack for setuptools_scm.
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+    If that issue is resolved, this method can be removed.
+    """
+    return ""
+
+
 setup(
     name='cpi',
-    version='1.0.0',
     description="Quickly adjust U.S. dollars for inflation using the Consumer Price Index (CPI)",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
@@ -52,4 +78,6 @@ setup(
         "Source": "https://github.com/palewire/cpi>",
         "Tracker": "https://github.com/palewire/cpi/issues",
     },
+    setup_requires=["pytest-runner", "setuptools_scm"],
+    use_scm_version={"version_scheme": version_scheme, "local_scheme": local_version},
 )
