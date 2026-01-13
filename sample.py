cbf0c771fabf21de43652141465fdf564db473ad
palewire <b@palewi.re>
2026-01-13 09:07:40 -0500

    Update pre-commit hooks and data files for December 2025

--- sample.py
+++ sample.py
@@ -2,8 +2,12 @@ import pandas as pd
 
 import cpi
 
-pd.DataFrame([o.__dict__() for o in cpi.areas.all()]).to_csv("./data/areas.csv", index=False)
-pd.DataFrame([o.__dict__() for o in cpi.items.all()]).to_csv("./data/items.csv", index=False)
+pd.DataFrame([o.__dict__() for o in cpi.areas.all()]).to_csv(
+    "./data/areas.csv", index=False
+)
+pd.DataFrame([o.__dict__() for o in cpi.items.all()]).to_csv(
+    "./data/items.csv", index=False
+)
 pd.DataFrame([o.__dict__() for o in cpi.periods.all()]).to_csv(
     "./data/periods.csv", index=False
 )
